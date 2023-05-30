822ae515eb29e8b6faf5e328dfa8881a4f594bd7
your_name <patip8999@gmail.com>
2023-05-30 11:03:34 +0200

    New

---
+++ src/app/components/navbar/navbar.component-module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  imports: [],
+  declarations: [NavbarComponent],
+  providers: [],
+  exports: [NavbarComponent]
+})
+export class NavbarComponentModule {
+}
