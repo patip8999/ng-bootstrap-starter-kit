9c0321608dad35b7bf96f2ccad03572e8210587c
your_name <patip8999@gmail.com>
2023-05-31 12:10:22 +0200

    works

--- src/app/components/team-detail/team-detail.component-module.ts
+++ src/app/components/team-detail/team-detail.component-module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { TeamDetailComponent } from './team-detail.component';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { DateFormatPipeModule } from "../../pipes/date-format/date-format.pipe-module";
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [TeamDetailComponent],
-  providers: [],
-  exports: [TeamDetailComponent]
+    declarations: [TeamDetailComponent],
+    providers: [DateFormatPipeModule],
+    exports: [TeamDetailComponent],
+    imports: [CommonModule, RouterModule, DateFormatPipeModule]
 })
 export class TeamDetailComponentModule {
 }
--- src/app/components/team-detail/team-detail.component.ts
+++ src/app/components/team-detail/team-detail.component.ts
@@ -6,7 +6,9 @@ import {
 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
+import { ProjectModel } from 'src/app/models/project.model';
 import { TeamModel } from 'src/app/models/team.model';
+import { ProjectsService } from 'src/app/services/projects.service';
 import { TeamsService } from 'src/app/services/teams.service';
 
 @Component({
@@ -20,21 +22,23 @@ export class TeamDetailComponent implements OnInit{
   teamId: string | null = null;
   TeamDetail$: Observable<TeamModel> = this._teamsService.getOne('id');
   Avatars$: Observable<TeamModel[]> = this._teamsService.getAll();
+  Projects$: Observable<ProjectModel>= this._projectsService.getOne('id')
   constructor(
     private _teamsService: TeamsService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private _projectsService: ProjectsService
   ) {}
 
 
 
-
   ngOnInit() {
-    this.route.paramMap.subscribe((params) => {
+    this.route.paramMap.subscribe(params => {
       this.teamId = params.get('id');
 
       if (this.teamId !== null) {
         this.TeamDetail$ = this._teamsService.getOne(this.teamId);
         this.Avatars$ = this._teamsService.getAll();
+        this.Projects$ = this._projectsService.getOne(this.teamId);
       }
     });
   }
