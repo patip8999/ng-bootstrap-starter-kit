23ce57cee10cab70efb4668c0b2af91da9df76eb
your_name <patip8999@gmail.com>
2023-06-01 09:53:53 +0200

    almost

--- src/app/components/employee-detail/employee-detail.component.ts
+++ src/app/components/employee-detail/employee-detail.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { EmployeeModel } from 'src/app/models/employee.model';
+import { ProjectModel } from 'src/app/models/project.model';
 import { TeamModel } from 'src/app/models/team.model';
 import { EmployeesService } from 'src/app/services/employees.service';
+import { ProjectsService } from 'src/app/services/projects.service';
 import { TeamsService } from 'src/app/services/teams.service';
 
 @Component({
@@ -18,23 +20,34 @@ export class EmployeeDetailComponent  implements OnInit {
 Detail$: Observable<EmployeeModel>= this.employeesService.getOne('id')
 Team$: Observable<TeamModel>= this._teamService.getOne('id')
 TeamAvatars$: Observable<TeamModel[]>=this._teamService.getAll()
+Projects$: Observable<ProjectModel[]>=this._projectsService.getAll()
   constructor(
 
 
     private employeesService: EmployeesService,
     private route: ActivatedRoute,
-    private _teamService: TeamsService
+    private _teamService: TeamsService,
+    private _projectsService: ProjectsService
   ) { }
 
-  
+  public selectedTabSubject = new Subject<string>();
+  selectedTab$ = this.selectedTabSubject.asObservable();
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       const employeeId = params.get('id');
       if (employeeId !== null) {
         this.Detail$ = this.employeesService.getOne(employeeId);
         this.Team$ = this._teamService.getOne(employeeId); 
+        this.Projects$ = this._projectsService.getAll();
         this.TeamAvatars$ = this._teamService.getAll()
       }
     });
+  }
+  selectTeamsTab() {
+    this.selectedTabSubject.next('teams');
+  }
+  
+  selectProjectsTab() {
+    this.selectedTabSubject.next('projects');
   }
 }
